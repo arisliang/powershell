47594a9901598d073ff762347fc350b8f3367235
Aris Liang <arisliang@navig8group.com>
2015-06-24 12:25:25 +0800

    adds loc count script.

--- ListLinesOfCodes.ps1
+++ ListLinesOfCodes.ps1
@@ -1,2 +1,5 @@
 # http://stackoverflow.com/questions/11561497/powershell-script-to-count-number-of-lines-ommitting-a-line-containing-a-specifi
 dir c:\myfolder -include *.cs,*.xaml,*.txt -Recurse | % { $count = (gc $_ |  ? { $_ -notmatch '^\s*$|^''|/\*|\*/' }).count; if ($count -gt 900) {write-host "$_ `tcount: $count"} }
+
+#http://www.limilabs.com/blog/source-lines-of-code-count-using-powershell
+(dir -include *.cs -recurse | select-string "^(\s*)//" -notMatch | select-string "^(\s*)$" -notMatch | select-string "^\s*(?:\{|\})\s*$" -notMatch).Count
